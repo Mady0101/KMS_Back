da08924821bea0a9e82ffaa0ab347daa7f08291c
Mady0101 <78955134+Mady0101@users.noreply.github.com>
2022-07-13 20:31:51 +0200

    Create TwilioSendSmsService.java

---
+++ src/main/java/com/example/kms/Service/TwilioSendSmsService.java
@@ -0,0 +1,35 @@
+package com.example.kms.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+
+@Service
+public class TwilioSendSmsService {
+	
+	 	@Value("${twilio.accountSid}")
+	    private String ACCOUNT_SID;
+
+	    @Value("${twilio.authToken}")
+	    private String AUTH_TOKEN;
+
+	    @Value("${twilio.trialNumber}")
+	    private String FROM_NUMBER;
+
+	    public void send(String to , String msg) {
+	        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+	        Message message = Message.creator(new PhoneNumber(to), new PhoneNumber(FROM_NUMBER), msg)
+	                .create();
+	        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
+
+	    }
+
+		
+
+}
+
